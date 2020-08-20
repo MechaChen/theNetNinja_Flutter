d9db01257a5ebb34383e8a1cb629ae34644cb9d7
Benson Chen <tumlivein@gmail.com>
2020-08-20 15:10:36 +0800

    4. Creating a Flutter App in Android Studio

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('first Flutter App'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('hello ninjas'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home: Text('hey ninjas!'),
 ));
