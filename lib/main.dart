d0eef8a940979760c1ebeb61231dc03fb6e81330
Benson Chen <tumlivein@gmail.com>
2020-09-18 11:14:00 +0800

    23. Maps & Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/home.dart';
+import 'package:myapp/pages/loading.dart';
+import 'package:myapp/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
 
